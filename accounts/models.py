98de06fa06fc49658dad29b174d8d61e79866b59
smaman1314 <smaman1314@gmail.com>
2020-09-25 23:45:19 +0530

    StaticLoadAndModel

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=30, null=True)
+    phone = models.CharField(max_length=30, null=True)
+    email = models.CharField(max_length=30, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor')
+    )
+    name = models.CharField(max_length=30, null=True)
+    price = models.FloatField(null=True)
+    category = models.chaname = models.CharField(max_length=30, null=True, choices=CATEGORY)
+    description = models.chaname = models.CharField(max_length=30, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for Delivery', 'Out for Delivery'),
+        ('Delivered', 'Delivered')
+    )
+    # cutomer = 
+    # product = 
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=30, choices=STATUS, null=True)
--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home),
     path('products', views.products),
     path('customers', views.customers),
