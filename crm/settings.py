b1dc4755a980a5c3203ad14c70f95e96e99c5b40
smaman1314 <smaman1314@gmail.com>
2020-10-01 19:02:33 +0530

    ResetPassword

--- crm/settings.py
+++ crm/settings.py
@@ -118,3 +118,10 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
